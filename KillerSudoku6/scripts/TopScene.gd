aa33a004b29fe88e7263dbc6ca3780847daf04b8
vivisuke <ntsuda@master.email.ne.jp>
2022-02-11 02:30:11 +0900

    0051 ◎ C 02/10 02/11 0.001│ToDo│問題集ボタン押下 → レベルシーンに遷移

---
+++ KillerSudoku6/scripts/TopScene.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var buttons = []
+onready var g = get_node("/root/Global")
+
+func _ready():
+	pass # Replace with function body.
+
+
+func to_LevelScene(qLevel):
+	#print($LineEdit.text)
+	g.qLevel = qLevel
+	g.qName = ""
+	g.qRandom = false	#$LineEdit.text == ""
+	g.todaysQuest = false
+	get_tree().change_scene("res://LevelScene.tscn")
+func _on_Button3_pressed():
+	to_LevelScene(0)
+func _on_Button4_pressed():
+	to_LevelScene(1)
+func _on_Button5_pressed():
+	to_LevelScene(2)
