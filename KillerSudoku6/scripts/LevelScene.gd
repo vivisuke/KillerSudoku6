5cec0b1c36ecb796640a9b8fffa45769380fb23c
vivisuke <ntsuda@master.email.ne.jp>
2022-02-11 02:37:00 +0900

    0053 ◎ C 02/11 02/11 0.001│ToDo│レベル画面：難易度＋「問題集」をタイトルバーに表示

--- KillerSudoku6/scripts/LevelScene.gd
+++ KillerSudoku6/scripts/LevelScene.gd
@@ -1,7 +1,19 @@
 extends Node2D
 
+const N_SOLVED_QUEST = 10		# クリア済み問題数
+
+var autoScrolled = false
+var panels = []
+
+onready var g = get_node("/root/Global")
 
 func _ready():
+	var txt = ""
+	if g.qLevel == 0: txt = "入門"
+	elif g.qLevel == 1: txt = "初級"
+	elif g.qLevel == 2: txt = "初中級"
+	txt += "問題集"
+	$TitleBar/Label.text = txt
 	pass # Replace with function body.
 
 func _on_BackButton_pressed():
