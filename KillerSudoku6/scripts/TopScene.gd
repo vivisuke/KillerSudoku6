d6e8a46db0317139931aab85979ad7f2316badf4
vivisuke <ntsuda@master.email.ne.jp>
2022-02-15 07:45:07 +0900

    0093 ◎ C 02/14 02/15 0.001│ToDo│トップ画面：問題生成ボタン押下→メイン画面遷移

--- KillerSudoku6/scripts/TopScene.gd
+++ KillerSudoku6/scripts/TopScene.gd
@@ -34,6 +34,20 @@ func _ready():
 		buttons[i].get_node("BestTimeLabel").text = txt
 	pass # Replace with function body.
 
+func to_MainScene(qLevel):
+	print($LineEdit.text)
+	g.qLevel = qLevel
+	g.qName = $LineEdit.text
+	g.qRandom = $LineEdit.text == ""
+	g.qNumber = 0
+	g.todaysQuest = false
+	get_tree().change_scene("res://MainScene.tscn")
+func _on_Button0_pressed():
+	to_MainScene(0)
+func _on_Button1_pressed():
+	to_MainScene(1)
+func _on_Button2_pressed():
+	to_MainScene(2)
 
 func to_LevelScene(qLevel):
 	#print($LineEdit.text)
@@ -48,3 +62,4 @@ func _on_Button4_pressed():
 	to_LevelScene(1)
 func _on_Button5_pressed():
 	to_LevelScene(2)
+
